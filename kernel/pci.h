6d4052816d72ba93df10852287e253b5eded3d23
Thomas Moore <tmoo@cs.utexas.edu>
2022-04-29 11:47:46 -0500

    bruh

--- kernel/pci.h
+++ kernel/pci.h
@@ -1,13 +1,23 @@
 #ifndef _pci_h
 #define _pci_h
 
+#include "machine.h"
+#include "idt.h"
+#include "libk.h"
+#include "config.h"
+#include "debug.h"
+#include "stdint.h"
+
+
 // this code is from osdev
 
 namespace PCI {
 
-    uint16_t pciConfigReadWord(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {};
+    void init();
 
-    uint16_t pciCheckVendor(uint8_t bus, uint8_t slot) {};
+    uint16_t pciConfigReadWord(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+
+    uint16_t pciCheckVendor(uint8_t bus, uint8_t slot);
 
 }
 
