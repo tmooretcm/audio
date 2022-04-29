eeb3c8c085e84d637031f1a8525247e7c5f788c0
Thomas Moore <tmoo@cs.utexas.edu>
2022-04-29 14:26:39 -0500

    .h done

--- kernel/pci.cc
+++ kernel/pci.cc
@@ -2,46 +2,11 @@
 
 namespace PCI {
 
-    #define BAR0 0x10
-    #define BAR1 0x14
-    #define BAR2 0x18
-    #define BAR3 0x1C
-    #define BAR4 0x20
-    #define BAR5 0x24
+    pci_device** pci_devices = 0;
+    uint32_t devices = 0;
 
-    struct PCIDevice {
-        uint16_t vendor_id;
-        uint16_t device_id;
-        uint16_t command;
-        uint16_t status;
-        uint8_t rev_id;
-        uint8_t prog_if;
-        uint8_t subclass;
-        uint8_t class_code;
-        uint8_t cache_line_size;
-        uint8_t latency_timer;
-        uint8_t header_type;
-        uint8_t bist;
-        uint32_t bar0;
-        uint32_t bar1;
-        uint32_t bar2;
-        uint32_t bar3;
-        uint32_t bar4;
-        uint32_t bar5;
-        uint32_t cardbus_cis_pointer;
-        uint16_t subsystem_vendor_id;
-        uint16_t subsystem_id;
-        uint32_t expansion_rom_base;
-        uint8_t capabilities;
-        char* reserved[56];
-        uint8_t int_line;
-        uint8_t int_pin;
-        uint8_t min_grant;
-        uint8_t max_latency;
-    };
-
-    void init() {
-    } 
+    pci_driver** pci_drivers = 0;
+    uint32_t drivers = 0;
     
     uint16_t pciConfigReadWord(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
         uint32_t address;
