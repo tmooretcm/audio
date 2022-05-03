bc7502c02cf7634c63f4e72b7ba56f74c495a1d0
Thomas Moore <tmoo@cs.utexas.edu>
2022-05-03 18:33:12 -0500

    corb_write

--- kernel/audio.cc
+++ kernel/audio.cc
@@ -6,6 +6,7 @@ static audio_driver* driver;
 // update interrupt status
 using namespace audio;
 
+// Handles any interrupts - not technically needed to implement audio but since we have interrupts enabled we must have this
 static void handle_interrupt(hda_audio_device* device) {
     // Grab current values
     uint32_t curr_int_sts = REG_INL(device, REG_INTSTS);
@@ -25,6 +26,7 @@ static void handle_interrupt(hda_audio_device* device) {
     REG_OUTB(device, REG_O0_STS, curr_out_sts);
 }
 
+// Initialize the CORB data structure
 static void init_corb(hda_audio_device* device) {
     uint8_t reg;
     uint32_t corb_base_addr;
@@ -49,13 +51,14 @@ static void init_corb(hda_audio_device* device) {
 
     // initialize corb base address
     corb_base_addr = (uintptr_t)device->rings->pa[0];
-    REG_OUTL(device, REG_CORBLBASE, corb_base_addr & 0xffffffff);
+    REG_OUTL(device, REG_CORBLBASE, corb_base_addr & 0xFFFFFFFF);
     REG_OUTL(device, REG_CORBUBASE, corb_base_addr >> 32);
 
     // start DMA
     REG_OUTB(device, REG_CORBCTL, 0x02);
 }
 
+// Initialize the RIRB data structure
 static void init_rirb(hda_audio_device* device) {
     uint8_t reg;
     uint32_t rirb_base_addr;
@@ -78,7 +81,7 @@ static void init_rirb(hda_audio_device* device) {
     REG_OUTB(device, REG_RIRBSIZE, reg);
 
     rirb_base_addr = (uintptr_t)device->rings->pa[0] + 1024; // should be pa[256] (?)
-    REG_OUTL(device, REG_RIRBLBASE, rirb_base_addr & 0xffffffff);
+    REG_OUTL(device, REG_RIRBLBASE, rirb_base_addr & 0xFFFFFFFF);
     REG_OUTL(device, REG_RIRBUBASE, rirb_base_addr >> 32);
 
     // set interrupt cnt register
@@ -86,4 +89,22 @@ static void init_rirb(hda_audio_device* device) {
 
     // start DMA
     REG_OUTB(device, REG_RIRBCTL, 0x02);
+}
+
+// Write the given verb to the CORB at the next available spot
+static void corb_write(hda_audio_device* device, uint32_t verb) {
+    uint16_t write_pointer = REG_INW(device, REG_CORBWP) & 0xFF;
+    uint16_t read_pointer, next;
+
+    // next available spot in corb - take modulus for the ring 
+    next = (write_pointer + 1) % device->corb_entries;
+
+    // wait until there's space in corb to write 
+    do {
+        read_pointer = REG_INW(device, REG_CORBRP) & 0xFF;
+    } while (next == read_pointer);
+
+    // found space, write to corb
+    device->corb[next] = verb;
+    REG_OUTW(device, REG_CORBWP, next);
 }
